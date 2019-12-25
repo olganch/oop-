413a68674608c6af0c57e61bbe06345cddbb82fc
lyoleech <59200942+lyoleech@users.noreply.github.com>
2019-12-25 19:25:03 +0300

    Add files via upload

---
+++ lr1/nuber123.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//три функции, не возвращающие значения
+
+namespace Lab_1
+{
+    class Program
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Morning()
+        {
+            Console.WriteLine("Good morning!");
+        }
+        public static void Afternoon()
+        {
+            Console.WriteLine("Good afternoon!");
+        }
+
+        int Summa(int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+
+        static Program S = new Program();
+        static void Main(string[] args)
+        {
+            int a = 9, b = 5;
+            Hello();
+            Morning();
+            Afternoon();
+            Console.WriteLine("Summa = {0}", S.Summa(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
